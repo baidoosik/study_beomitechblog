18eea6822795a0092a7e5a80f1a797d60ca8d427
baidoosik <qoentlr37@naver.com>
2017-09-10 01:37:01 +0900

    ajax 멜론 예제

--- youtube/views.py
+++ youtube/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'youtube/askdjango3.html')
+    return render(request,'youtube/askdjango_ajax.html')
